aa5a19632cb5b5e4dea4ac7e5b49a56f940fa7ff
babyloos <babyloos1990@yahoo.co.jp>
2017-11-07 11:51:10 +0000

    利益計算追加

--- app/controllers/concerns/arbitrage.rb
+++ app/controllers/concerns/arbitrage.rb
@@ -8,17 +8,46 @@
 =end
 module Arbitrage
     extend ActiveSupport::Concern # インスタンスメソッドのみの利用時は記述の必要なし
+    # require 'net/http'
+    # require 'uri'
     
-    # 各種価格の取得
-    class GetValue
-        
-        def self.coincheckBid
-            
+    class DataUpdate
+        include Zaif
+        def initialize
+            @coincheckApi = CoincheckClient.new("YOUR API KEY", "YOUR SECRET KEY")
+            @zaifApi = Zaif::API.new()
         end
         
-        def self.coincheckAsk
+        def update
+            response = @coincheckApi.read_ticker
+            coincheckData = JSON.parse(response.body)
+            zaifData = @zaifApi.get_ticker("btc")
+            asset = Value.new(coincheck_bid: coincheckData["bid"], coincheck_ask: coincheckData["ask"], zaif_bid: zaifData["bid"], zaif_ask: zaifData["ask"])
+            asset.save
         end
+        
+        private
         
     end
+
+    class ProfitCalc
+        def initialize
+            @value = Value.last
+        end
+        
+        def profit
+            {buy_coincheck: @value.zaif_bid - @value.coincheck_ask, buy_zaif: @value.coincheck_bid - @value.zaif_ask}
+        end
+    end
+    
+    private
+    
+    # 各種価格の取得
+    
+    # def getCoincheckValue
+    #     cc = CoincheckClient.new("YOUR API KEY", "YOUR SECRET KEY")
+    #     response = cc.read_ticker
+    #     JSON.parse(response.body)
+    # end
     
 end
--- app/controllers/trade_controller.rb
+++ app/controllers/trade_controller.rb
@@ -2,6 +2,18 @@ class TradeController < ApplicationController
     include Arbitrage
     def home
         @asset = Asset.last
-        sayHello
+        @value = Value.last
+        
+        p = ProfitCalc.new
+        @profit = p.profit
+        
+        # update = DataUpdate.new
+        # update.update
+        # c = Coincheck.new
+        # z = Za.new
+        # @coincheckValue = c.getValue
+        # @zaifValue = z.getValue
+        # byebug
+        # @value = {coincheck_bid: value["bid"], coincheck_ask: value["ask"], zaif_bid: }
     end
 end
